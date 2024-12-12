9e523b0aaecfc6922d30a58a1db9741d70f9b846
Adesh Shrivastav <157503710+AdeshShrivastav@users.noreply.github.com>
2024-12-12 13:45:59 +0530

    app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ model_id1 = "dreamlike-art/dreamlike-diffusion-1.0"
 model_id2 = "stabilityai/stable-diffusion-xl-base-1.0"
 
 pipe = StableDiffusionPipeline.from_pretrained(model_id1, torch_dtype=torch.float16, use_safetensors=True)
-pipe = pipe.to("cuda")
+# pipe = pipe.to("cuda")
 
 
 
